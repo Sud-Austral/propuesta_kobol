67071643b2010573dc425d3319d68fea8a99fdfb
lmonsalve22 <lmonsalve22@gmail.com>
2024-01-30 18:26:54 -0300

    try en el action

--- automatizacion.py
+++ automatizacion.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     url_api = f"https://kf.kobotoolbox.org/api/v2/assets/{form_id}/data.json"
     https://kf.kobotoolbox.org/api/v2/assets/a93vkSiKSw6kbWy5xtuT5e/data.json
     headers = {'Authorization': f'Token f90946e51dce6fa9445df9ed06d2349fb4081113'}
-    
+
     response = req.get(url_api, headers=headers)
     print(response.status_code)
     if response.status_code == 200:
@@ -22,7 +22,10 @@ if __name__ == '__main__':
         for fe in respuesta["results"]:
             baseProperties = fe.copy()
             baseGeometry["type"] = "MultiPolygon"
-            baseGeometry["coordinates"] = [[[[float(x.split(" ")[1]),float(x.split(" ")[0])] for x in fe["Extensi_n_territoria_nar_metros_cuadrados"].split(";")]]]
+            try:
+                baseGeometry["coordinates"] = [[[[float(x.split(" ")[1]),float(x.split(" ")[0])] for x in fe["Extensi_n_territoria_nar_metros_cuadrados"].split(";")]]]
+            except:
+                pass
             baseFeature["properties"] = baseProperties.copy()
             baseFeature["geometry"] = baseGeometry.copy()
             base["features"].append(baseFeature.copy())
