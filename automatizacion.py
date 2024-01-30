11966443dee074cac62aeeefc554f24cfd32e3e9
lmonsalve22 <lmonsalve22@gmail.com>
2024-01-30 17:26:21 -0300

    error request

--- automatizacion.py
+++ automatizacion.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     form_id = 'a93vkSiKSw6kbWy5xtuT5e'  # Reemplaza esto con el ID de tu formulario
     url_api = f"https://kf.kobotoolbox.org/api/v2/assets/{form_id}/data.json"
     headers = {'Authorization': f'Token f90946e51dce6fa9445df9ed06d2349fb4081113'}
-    response = requests.get(url_api, headers=headers)
+    response = req.get(url_api, headers=headers)
     print(response.status_code)
     if response.status_code == 200:
         respuesta = response.json()
